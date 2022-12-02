0b89aa21583268cf2dd74ad0abae5bc29f381f8c
jiangqb <jiangqb@citycloud.com.cn>
2022-12-02 14:10:47 +0800

    feat: 自定义 401 错误

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: jiangqb jiangqb@citycloud.com.cn
+ * @Date: 2022-12-01 08:58:23
+ * @LastEditors: jiangqb jiangqb@citycloud.com.cn
+ * @LastEditTime: 2022-12-02 14:06:06
+ * @FilePath: /vue3-admin-server/src/app.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BDwei
+ */
 import Koa from "koa"
 import cors from "@koa/cors"
 import logger from "koa-logger"
@@ -5,6 +13,8 @@ import bodyparser from "koa-bodyparser"
 // routes
 import authRoutes from "./routes/auth"
 import "./db"
+import jwt from "koa-jwt"
+import { jwtSecret } from "./config/auth"
 
 // koa 应用实例
 const app = new Koa()
@@ -18,6 +28,28 @@ app.use(
   })
 )
 app.use(logger()) // 开发日志中间件
+
+app.use((ctx, next) => {
+  return next().catch(err => {
+    if (err.status === 401) {
+      ctx.status = 401
+      ctx.body = {
+        code: 401,
+        error: "未登录 token 失效"
+      }
+    } else {
+      ctx.throw(err)
+    }
+  })
+})
+
+// token 验证 header 未携带 token 直接返回 401 Authentication Error
+app.use(
+  jwt({ secret: jwtSecret }).unless({
+    // 白名单
+    path: ["/api/auth/login", "/api/auth/register"]
+  })
+)
 
 // routes
 // 用户验证路由（登录 注册）
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: jiangqb jiangqb@citycloud.com.cn
+ * @Date: 2022-12-02 09:23:39
+ * @LastEditors: jiangqb jiangqb@citycloud.com.cn
+ * @LastEditTime: 2022-12-02 14:07:31
+ * @FilePath: /vue3-admin-server/src/routes/auth.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import Router from "@koa/router";
 import { loginController, LoginModel, registerController } from "../controller/auth"
 import { UserAttributes } from "../db/models/User.model";
@@ -21,6 +29,10 @@ router.post("/register", async (ctx) => {
  */
 router.post("/login", async(ctx) => {
   ctx.body = await loginController(ctx.request.body as LoginModel)
+})
+
+router.get("/test", async (ctx) => {
+  ctx.body = "测试接口"
 })
 
 export default router;
